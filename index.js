8a87443b21008c984e826fca37b70c31fc78f8e8
Timna <timnar312@gmail.com>
2021-08-20 17:45:24 +0300

    Fix: Notes auto checker - Profile BEM

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let editButton = document.querySelector(".info__edit");
+let editButton = document.querySelector(".profile__edit");
 let popupForm = document.querySelector(".popup");
 let closeBotton = document.querySelector(".popup__close-button");
 let form = document.querySelector(".form");
 let inputName = document.querySelector(".form__input_type_name");
 let inputAbout = document.querySelector(".form__input_type_about");
-let profileName = document.querySelector(".info__name");
-let profileAbout = document.querySelector(".info__about");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
 
 function closeForm() {
     popupForm.classList.remove("popup_open")
