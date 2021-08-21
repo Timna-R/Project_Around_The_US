f52ab645ac0b6fe7617b5b9c4775cd2a0900172f
Timna <timnar312@gmail.com>
2021-08-21 23:53:14 +0300

    hotfix: Improving code from comments

--- index.js
+++ index.js
@@ -7,25 +7,31 @@ let inputAbout = document.querySelector(".form__input_type_about");
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
 
-function closeForm() {
-    popupForm.classList.remove("popup_open")
+function openPopUp() {
+    popupForm.classList.add("popup_open");
+    const nameValue = profileName.textContent;
+    const aboutValue = profileAbout.textContent;
+    inputName.value = nameValue;
+    inputAbout.value = aboutValue;
 }
 
-editButton.addEventListener("click", function() {
-    popupForm.classList.add("popup_open");
-    const nameValue = profileName.textContent
-    const aboutValue = profileAbout.textContent
-    inputName.value = nameValue
-    inputAbout.value = aboutValue
-})
+function sendForm() {
+    const nameValue = inputName.value;
+    const aboutValue = inputAbout.value;
+    profileName.textContent = nameValue;
+    profileAbout.textContent = aboutValue;
+}
+
+function closeForm() {
+    popupForm.classList.remove("popup_open");
+}
+
+editButton.addEventListener("click", openPopUp)
 
 closeBotton.addEventListener("click", closeForm)
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    const nameValue = inputName.value
-    const aboutValue = inputAbout.value
-    profileName.textContent = nameValue
-    profileAbout.textContent = aboutValue
+    sendForm()
     closeForm()
 })
